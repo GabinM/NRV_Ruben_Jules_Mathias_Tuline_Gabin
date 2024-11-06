a5297793f8a20684524652d530dea2ce26bdbb60
GabinM <gabin.mathieu54@gmail.com>
2024-11-06 17:43:58 +0100

    added NRVRepository based on DeefyRepository

--- Src/Classes/Repository/NRVRepository.php
+++ Src/Classes/Repository/NRVRepository.php
@@ -7,16 +7,32 @@ use PDO;
 class NRVRepository
 {
 
-    private $pdo;
+    private static SpautifyRepository $sp;
+    private static string $CONF;
+    private static array $PARAMS;
+    private PDO $bd;
 
-    public function __construct()
-    {
-        $config = parse_ini_file('db.config.ini');
-
-        if (!$config || !isset($config['host'], $config['dbname'], $config['user'], $config['password'])) {
-            die("Fichier de configuration manquant ou incomplet.");
+    private function __construct(){
+        if(!isset($_PARAMS)){
+            $this->setConfig("conf.ini");
         }
+        $this->bd =  new PDO(self::$PARAMS['driver'].":host=".self::$PARAMS['host']."; dbname=".self::$PARAMS['database']."; charset=utf8", self::$PARAMS['username'],self::$PARAMS['password']);
     }
 
+    public static function setConfig($file){
+        self::$CONF = $file;
+        self::$PARAMS = parse_ini_file($file);
+    }
+
+    public static function getInstance(){
+        if(! isset($sp)){
+            $sp = new SpautifyRepository();
+        }
+        return $sp;
+    }
+
+    public function getDb(){
+        return $this->bd;
+    }
 
 }
