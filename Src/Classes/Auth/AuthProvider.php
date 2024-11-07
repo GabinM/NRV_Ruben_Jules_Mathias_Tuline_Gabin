1fcce94933cad75c98f2e15a77aac2d68cd7b7b2
tuline-leveque <tuline.leveque6@etu.univ-lorraine.fr>
2024-11-07 16:54:53 +0100

    correction AuthProvider

--- Src/Classes/Auth/AuthProvider.php
+++ Src/Classes/Auth/AuthProvider.php
@@ -2,30 +2,45 @@
 
 namespace nrv\Auth;
 
+use nrv\Exceptions\AuthzException;
+use nrv\repository\NRVRepository;
+use PDO;
+
 class AuthProvider{
-    
+
+    /**
+     * @throws AuthzException
+     */
     public static function signin($addr, $mdp){
-        $conn = \nrv\Repository\NRVRepository::getInstance()->getDb();
+        $conn = NRVRepository::getInstance()->getDb();
 
         $query = $conn->prepare("select hashmdp from utilisateur where email = ? ;");
         $query->bindParam(1,$addr);
         $query->execute();
 
+        $query2 = $conn->prepare("select count(*) from utilisateur where email = ? ;");
+        $query2->bindParam(1,$addr);
+        $query2->execute();
+
+        if ($query2->fetch(PDO::FETCH_ASSOC)["count(*)"] == 0){
+            throw new AuthzException("identifiant ou mot de passe invalide");
+        }
+
         $pwd = $query->fetch(PDO::FETCH_ASSOC)["hashmdp"];
 
-        if (! password_verify($mdp,$pwd)){
-            throw new \nrv\Exception\AuthzException("identifiant ou mot de passe invalide");
+        if (!password_verify($mdp,$pwd)){
+            throw new AuthzException("identifiant ou mot de passe invalide");
         }
     }
 
     /**
-     * @throws AuthnException
+     * @throws AuthzException
      */
     public static function register($addr, $mdp){
         if (strlen($mdp) < 10 ){
-            throw new \spautify\exception\AuthnException("mot de passe trop faible");
+            throw new AuthzException("mot de passe trop faible");
         }
-        $conn = \spautify\repository\SpautifyRepository::getInstance()->getDb();
+        $conn = NRVRepository::getInstance()->getDb();
 
         $query = $conn->prepare("select * from utilisateur where email = ? ;");
         $query->bindParam(1,$addr);
@@ -34,7 +49,7 @@ class AuthProvider{
         $res = $query->fetchAll(PDO::FETCH_ASSOC);
 
         if (sizeof($res) != 0) {
-            throw new \spautify\exception\AuthnException("un utilisateur utilise déjà cette adresse");
+            throw new AuthzException("un utilisateur utilise déjà cette adresse");
         }
 
 
