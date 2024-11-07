66e3ddec42ac9c02fc50af34a490c2ec9ea29ad1
GabinM <gabin.mathieu54@gmail.com>
2024-11-07 09:45:26 +0100

    included Dispatcher in index

--- Src/index.php
+++ Src/index.php
@@ -6,5 +6,12 @@ if(! isset($_SESSION)){
     session_start();
 }
 
+if(isset($_REQUEST['action'])){
+    $d = new \spautify\Dispatcher($_REQUEST['action']);
+} else {
+    $d = new \spautify\Dispatcher("default");
+}
+
+$d->run();
 
 echo "<p>NRV</p>";
