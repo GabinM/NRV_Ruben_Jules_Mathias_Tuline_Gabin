882525ed65309ad967ab6a1e0bb84fd5bbc3d087
tuline-leveque <tuline.leveque6@etu.univ-lorraine.fr>
2024-11-07 11:25:03 +0100

    ActionDefault

---
+++ Src/Classes/Actions/ActionDefault.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace nrv\Actions;
+
+class ActionDefault extends Action {
+    public function execute() : string {
+        $html = "<a href='?action=display-soiree'> Afficher une soirée </a></br></br></br>";
+        $html .= "<a href='?action=create-soiree'> Créer une soirée </a></br></br></br>";
+
+        return $html;
+    }
+}
