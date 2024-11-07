8cd52789042fb8abc1482bc0efd1f5e986a13403
GabinM <gabin.mathieu54@gmail.com>
2024-11-07 12:36:12 +0100

    added checkRole function to Authz

--- Src/Classes/Auth/Authz.php
+++ Src/Classes/Auth/Authz.php
@@ -3,5 +3,24 @@
 namespace nrv\Auth;
 
 class Authz {
-    
+
+    public static function checkRole() {//retourne 0, 1 ou 2 en fonction des droits
+        $hash = unserialize($_SESSION['user'])['hash'];
+        if( $hash == ""){
+            
+            return 0; //droits basiques
+        } else {
+            $bd = \spautify\repository\SpautifyRepository::getInstance()->getDb();
+
+            $rlQuery = $bd->prepare("select role from user where hashmdp = ? ;");
+            $rlQuery->bindparam(1,$hash);
+            $rlQuery->execute();
+            $bdRl = $rlQuery->fetch(PDO::FETCH_ASSOC)['role'];
+
+            return $bdRl; // 1 : organisateur, 2 : admin
+
+        }
+        
+    }
+
 }
