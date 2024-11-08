2814fed6a38e7c517c9a1703f5904c9d830f7d6c
GabinM <gabin.mathieu54@gmail.com>
2024-11-08 10:21:09 +0100

    added ActionLinkToSpectacle

--- Src/Classes/Actions/ActionLinkSpectacleToSoiree.php
+++ Src/Classes/Actions/ActionLinkSpectacleToSoiree.php
@@ -35,38 +35,52 @@ class ActionLinkSpectacleToSoiree extends Action {
                         $likeName = "%".$_POST['soiree_name']."%";
                         $soireeQuery->bindParam(1, $likeName);
                         $soireeQuery->execute();
+                        $arr = $soireeQuery->fetchAll(PDO::FETCH_ASSOC);
     
                         $html .= "<form method='POST' action='?action=link-spectacle-soiree'>";
                         $html .= "<a>Soirées trouvées</a></br>";
-                        foreach($soireeQuery->fetch(PDO::FETCH_ASSOC) as $so){
-                            $html .= "<input type='radio' name='soir' value='{$so['id']}'><a>{$so['nomSoiree']} - {$so['date']}</a></br>";
+                        foreach($arr as $so){
+                            $html .= "<input type='radio' name='soir' value='{$so['idSoiree']}'><a>{$so['nomSoiree']} - {$so['date']}</a></br>";
                         }
     
                         $html .= "</br><a>Spectacles trouvés</a></br>";
     
-                        $spectacleQuery = $bd->prepare("select * from spectacle where nomSoiree like ? ;");
+                        $spectacleQuery = $bd->prepare("select * from spectacle where titre like ? ;");
                         $likeName = "%".$_POST['spectacle_name']."%";
                         $spectacleQuery->bindParam(1, $likeName);
                         $spectacleQuery->execute();
+                        $arr2 = $spectacleQuery->fetchAll(PDO::FETCH_ASSOC);
     
-                        foreach($spectacleQuery->fetch(PDO::FETCH_ASSOC) as $sp){
-                            $html .= "<input type='radio' name='spec' value='{$sp['id']}'><a>{$sp['nomSpectacle']} - {$sp['date']} par {$sp['nomArtistes']}</a></br>";
+                        foreach($arr2 as $sp){
+                            $html .= "<input type='radio' name='spec' value='{$sp['idSpectacle']}'><a>{$sp['titre']} - {$sp['date']} par {$sp['nomsArtistes']}</a></br>";
                         }
     
-                        $html .= "<input type='submit' value='enregistrer le lien' name = 'bind' ></form>";
+                        $html .= "<input type='submit' value='enregistrer le lien' name = 'bind' ></form></br>";
 
                     } 
                     
                 } else if(isset($_POST['bind'])){
                     $bd = \nrv\Repository\NRVRepository::getInstance()->getDb();
 
-                    $query = $bd->prepare("insert into spectacle2soiree(idSpectacle,idSoiree) values (?,?) ;");
-                    $query->bindParam(1, $_POST['spec']);
-                    $query->bindParam(2, $_POST['soir']);
-                    try{
-                        $query->execute();
-                    } catch(\Exception $e) {
-                        $html .= "<a>Erreur</a>";
+                    $checkQuery = $bd->prepare("select count(*) from spectacle2Soiree where idSpectacle = ? and idSoiree = ? ;");
+                    $checkQuery->bindParam(1, $_POST['spec']);
+                    $checkQuery->bindParam(2, $_POST['soir']);
+                    $checkQuery->execute();
+                    $res = $checkQuery->fetch(PDO::FETCH_ASSOC)['count(*)'];
+
+                    if($res > 0){
+                        $html .= "<a>Le spectacle est déjà relié à la soirée</a>";
+                    } else {
+                        $query = $bd->prepare("insert into spectacle2soiree(idSpectacle,idSoiree) values (?,?) ;");
+                        $query->bindParam(1, $_POST['spec']);
+                        $query->bindParam(2, $_POST['soir']);
+                        try{
+                            $query->execute();
+                            $html .= "<a>Spectacle relié à la soirée avec succès</a>";
+                        } catch(\Exception $e) {
+                            $html .= "<a>Erreur</a>";
+                            $html .= $e->getMessage();
+                        }
                     }
 
                 }
